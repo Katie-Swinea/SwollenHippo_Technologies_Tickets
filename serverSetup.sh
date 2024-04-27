270c91b06b5b42ca1fb62223acbf2fe12570f9d0
Katie Swinea <swineakatherine@gmail.com>
2024-04-27 18:04:21 +0000

    Updated serverSetup.sh to get ticket ID for each object.

--- serverSetup.sh
+++ serverSetup.sh
@@ -7,4 +7,16 @@
 
 strURLArray=$(curl https://www.swollenhippo.com/ServiceNow/systems/devTickets.php)
 #debug statment to test for curled objects
-echo ${strURLArray}
+#echo ${strURLArray}
+intArrayLength=$(echo ${strURLArray} | jq 'length')
+#debug statment to ensure the length has been determined
+echo $intArrayLength
+iteration=0
+
+while [ "$iteration" -lt "$intArrayLength" ];
+do
+strTicketID=$(echo ${strURLArray} | jq -r .[${iteration}].ticketID)
+#debug statement used to output the ticket Id was extracted for each object in the array
+echo $strTicketID
+((iteration++))
+done
