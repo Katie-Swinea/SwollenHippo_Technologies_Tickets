db895e9ae8ee74620a8eacaf1daf577cf9e73460
Katie Swinea <swineakatherine@gmail.com>
2024-04-27 20:02:46 +0000

    Updated serverSetup.sh to handle software installations.

--- serverSetup.sh
+++ serverSetup.sh
@@ -5,6 +5,8 @@
 #Author: Katie Swinea
 #Creation Date: 4/27/2024
 
+#used to update the machine once for software packages in the ticket
+sudo apt-get update
 strURLArray=$(curl https://www.swollenhippo.com/ServiceNow/systems/devTickets.php)
 #debug statment to test for curled objects
 #echo ${strURLArray}
@@ -38,9 +40,32 @@ echo "External IP Address: $2" >> configurationLogs/$strLogTitle
 strHostname=$(echo $HOSTNAME)
 #debug statement to ensure the hostname has been stored in the variable
 #echo $strHostname
+echo "Hostname: strHostname" >> configurationLogs/$strLogTitle
 strStandardConfig=$(echo ${strURLArray} | jq -r .[${iteration}].standardConfig)
 #debug staement to see the standard configuration for the file
 #echo $strStandardConfig
+strSoftwarePackages=$(echo ${strURLArray} | jq -r .[${iteration}].softwarePackages)
+intSoftwarePackages=$(echo ${strSoftwarePackages} | jq 'length')
+intSoftwareCheck=0
+
+#while loop is used to go through the software packages in the ticket if applicable
+while [ "$intSoftwareCheck" -lt "$intSoftwarePackages" ];
+do
+strTask=$(echo ${strSoftwarePackages} | jq -r .[$intSoftwareCheck].name)
+#debug statement to check the while loop functionality and display the software to be installed
+#echo $strTask
+strSoftwareInstall=$(echo ${strSoftwarePackages} | jq -r .[$intSoftwareCheck].install)
+#debug statement to ensure the install packages is being etracted properly
+#echo $strSoftwareInstall
+strTimeStamp=$(date +"%s")
+#debug statement to ensure the timestamp is correct
+#echo $strTimeStamp
+echo "$strTask - $strSoftwareInstall - $strTimeStamp" >> configurationLogs/$strLogTitle
+#used to install the specified packages
+sudo apt-get install $strSoftwareInstall -y
+((intSoftwareCheck++))
+done
+
 fi
 
 ((iteration++))
